4c604365a26a482e1ed8b9bfce1c7d07b67feb62
Irsannerha <irsanrh275@gmail.com>
2022-10-05 21:23:51 +0700

    TASK 2

---
+++ TASK_2/action.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DATA</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+  
+  <form>
+    <table align="center"> 
+      <tr align="center">
+        <td><h2> 
+            <b>OUTPUT DATA PRODUCT</b>
+        </h2></td>
+    </tr>
+      <tr> 
+        <td>
+        <table border="0" cellpadding="0" cellspacing="10" align="center">
+          <tr>
+            <td>DETAIL PRODUCT</td>
+          </tr>
+          <tr>
+            <td><center><img src="https://cdn.idntimes.com/content-images/community/2022/09/img-20220908-152834-779c2639fac019accf02dae4b183db6b-72d15e59fb35e6051fea276f1d3b155b_600x400.jpg" width="300" height="200" /></center><center><b>ID 001 - BOOKS</b></center></td>
+          </tr>
+        </table>
+        <center><b>Price : Rp. 50.000</b></center>
+      </td>
+      </tr>
+    </table>
+  </form>
+
+</body>
+</html>
+
