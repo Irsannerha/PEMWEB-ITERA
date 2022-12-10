75dc7a34425b6f11eb8ce21e11a6421dc7da26bb
Irsan Romardi Harahap <44488538+Irsannerha@users.noreply.github.com>
2022-12-10 23:50:45 +0700

    Update Index.php

--- Praktikum Web/Tugas 6/Index.php
+++ Praktikum Web/Tugas 6/Index.php
@@ -4,25 +4,13 @@ function BilRomawi($dataAngka) {
 	 $root = '';
 	 
 	 $array = array('M' => 1000,
-	 'CM' => 900,
-	 'D' => 500,
-	 'CD' => 400,
-	 'C' => 100,
-	 'XC' => 90,
-	 'L' => 50,
-	 'XL' => 40,
-	 'X' => 10,
-	 'IX' => 9,
-	 'V' => 5,
-	 'IV' => 4,
-	 'I' => 1);
+	 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1);
 	 
-	 foreach($array as $roman => $value){
+	 foreach($array as $roman => $value) {
 	  $matches = intval($dataAngka/$value);
-	 
 	  $root .= str_repeat($roman,$matches);
-	 
 	  $dataAngka = $dataAngka % $value;
+	 
 	 }
 	 
 	 return $root;
